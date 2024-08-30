c01c65e004ba8c0c94712773d7236722bc0ddb36
Kemal Inecik <benkemalim@gmail.com>
2024-08-30 11:03:29 +0200

    input module

---
+++ sctram/input/_constants.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+from collections.abc import Iterable
+from typing import Dict, List, Union
+
+
+# Custom Typings
+InputGraphDictWithoutEdge = Dict[str, Iterable[tuple[str, str]]]
+InputGraphDictNoneEdge = Dict[str, Iterable[tuple[str, str, None]]]
+InputGraphDictWithEdge = Dict[str, List[tuple[str, str, dict]]]
+InputGraphPossibleTypes = Union[InputGraphDictWithoutEdge, InputGraphDictNoneEdge, InputGraphDictWithEdge]
+
+# Constants
+key_trajectories = "trajectories"
+edge_reserved_keys = [key_trajectories]
+node_reserved_keys = [key_trajectories]
